c2c4d2bde7ff624923c56ef2b783bec187d300eb
hrid1 <hridoycse1@gmail.com>
2025-12-07 00:36:27 +0600

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>404 - Page not found</h1>
+      <p>Sorry, we couldn’t find what you were looking for.</p>
+    </div>
+  );
+}

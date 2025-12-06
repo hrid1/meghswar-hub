7924239893794da2fb8c495f9f534fbdfa38c896
hrid1 <hridoycse1@gmail.com>
2025-12-07 00:43:50 +0600

    fix version of next

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,9 @@
 export default function NotFound() {
   return (
-    <div style={{ padding: 40 }}>
+    <div className="flex items-center justify-center h-screen" style={{ padding: 40 }}>
       <h1>404 - Page not found</h1>
       <p>Sorry, we couldn’t find what you were looking for.</p>
+      
     </div>
   );
 }
